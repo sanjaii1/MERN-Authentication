8c9004098878a2f0a848882add139b9a114bdde0
sanjaii1 <bsanjay235@gmail.com>
2024-12-04 17:04:01 +0530

    app running and db  connection successfully

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,22 @@ const dotenv = require("dotenv");
 dotenv.config({
   path: "./config.env",
 });
+const app=require('./app')
 
+const db = process.env.DB;
 //database
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("DB connection Successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const port =process.env.PORT || 3000
+
+  app.listen(port,()=>{
+    console.log(`App running on port ${port}`)
+  })
