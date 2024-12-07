19989be5ac70c4c0146b4549a4f41760e39f9212
sanjaii1 <bsanjay235@gmail.com>
2024-12-07 22:07:20 +0530

    configsendemail functionality

---
+++ backend/utils/email.js
@@ -0,0 +1,22 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (option) => {
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `"SANJAI DEV AUTH APP"<sanjay235@gmail.com>`,
+    to: option.email,
+    subject: option.subject,
+    html: option.html,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
